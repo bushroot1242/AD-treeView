7c91ff31c236c65203cca383ba68a9c91fc148a8
Ildar <dilmuha@mail.ru>
2018-03-17 10:21:38 +0300

    first commite
    bla bla

---
+++ ActiveDirectoryTree/ActiveDirectoryTree/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ActiveDirectoryTree
+{
+    partial class FADTreeView
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvAD = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tvAD
+            // 
+            this.tvAD.Location = new System.Drawing.Point(12, 12);
+            this.tvAD.Name = "tvAD";
+            this.tvAD.Size = new System.Drawing.Size(652, 557);
+            this.tvAD.TabIndex = 0;
+            this.tvAD.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvAD_NodeMouseDoubleClick);
+            // 
+            // FADTreeView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 603);
+            this.Controls.Add(this.tvAD);
+            this.Name = "FADTreeView";
+            this.Text = "Дерево Active Directory";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvAD;
+    }
+}
+
---
+++ ActiveDirectoryTree/ActiveDirectoryTree/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices;
+
+
+namespace ActiveDirectoryTree
+{
+    public partial class FADTreeView : Form
+    {
+        public FADTreeView()
+        {
+            InitializeComponent();
+            string[] Root = ReadRoot("");
+            /*получаем данные о корне*/
+            TreeNode MyNodes = new TreeNode()
+            {
+                Text = Root[0],
+                Name = Root[1]
+            };
+
+            /*читаем все под ветки передаем ID родителя*/
+            List<ADinfo> Child = ReadChilds(Root[1]);
+            /*создаем найденные ветки к нашей MyNodes*/
+
+            foreach (ADinfo N in Child)
+            {
+                /*создаем дочернюю ветку*/
+                TreeNode MyNodesChilds = new TreeNode()
+                {
+
+                    Text = N.Name,
+
+                    Name = N.GUID
+
+                };
+
+                /*добавляем ветку к корню*/
+
+                MyNodes.Nodes.Add(MyNodesChilds);
+
+            }
+
+            /*добавляем ветки к редеву*/
+
+            tvAD.Nodes.Add(MyNodes);
+
+        }
+
+        /*читаем параметры указанной ветки*/
+        string[] ReadRoot(string ID)
+        {
+            /*обьявляем обьект работы с AD*/
+            DirectoryEntry AD;
+            /*если ID будет пустым значит читаем корень, если не пустым значит читаем данные об обьекте который выбран*/
+            if (ID == "") AD = new DirectoryEntry(); else AD = new DirectoryEntry("LDAP://<GUID=" + ID + ">");
+            /*читаем свойства полученного элемента*/
+            /*обьявляем в массиве два параметра*/
+            string[] Dinfo ={
+                AD.Name, //будем хранить имя
+                AD.Guid.ToString() //будем хранить ID
+                             };
+            /*возвращаем данные в виде массива*/
+            return Dinfo;
+
+        }
+
+        List<ADinfo> ReadChilds(string ID)
+        {
+
+            List<ADinfo> Childs = new List<ADinfo>();
+            DirectoryEntry AD;
+            if (ID == "") AD = new DirectoryEntry(); else AD = new DirectoryEntry("LDAP://<GUID=" + ID + ">");
+            DirectoryEntries D = AD.Children;
+
+            /*создаем курсор для чтения потомков*/
+
+            System.Collections.IEnumerator Reed = D.GetEnumerator();
+
+            /*обьявляем что будем использовать структуру*/
+
+            ADinfo Param;
+
+            /*Двигаем курсор на следующего потомка*/
+
+            while (Reed.MoveNext())
+
+            {
+
+                /*получаем данные о ветке*/
+
+                DirectoryEntry Info = Reed.Current as DirectoryEntry;
+
+                Param.Name = Info.Name; //сохраняем имя
+
+                Param.GUID = Info.Guid.ToString(); //сохраняем номер
+
+                /*сохраняем данные в список*/
+
+                Childs.Add(Param);
+
+            }
+
+            return Childs;
+
+        }
+
+        private void tvAD_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+
+            /*получаем ветку по которому дернул пользователь*/
+
+            TreeNode s = tvAD.SelectedNode;
+
+            /*получаем список дочерних элементов*/
+
+            /*читаем все под ветки передаем ID родителя*/
+
+            List<ADinfo> Child = ReadChilds(s.Name);
+
+            /*очистим содержимое ветки */
+
+            s.Nodes.Clear();
+
+            /*создаем найденные ветки к нашей MyNodes*/
+
+            foreach (ADinfo N in Child)
+
+            {
+
+                /*создаем дочернюю ветку*/
+
+                TreeNode MyNodesChilds = new TreeNode()
+
+                {
+
+                    Text = N.Name,
+
+                    Name = N.GUID
+
+                };
+
+                /*добавляем ветку к выбранному корню*/
+
+                s.Nodes.Add(MyNodesChilds);
+
+            }
+
+        }
+    }
+
+    struct ADinfo
+
+    {
+
+        public string Name, GUID;
+
+    }
+}
